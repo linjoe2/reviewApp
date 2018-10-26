b8fa04d96b0230955e5c248736f78af66cc50891
Lindsey Schaap <ik@Lindseyschaap.nl>
2018-10-26 16:44:18 +0200

    after lecture initial upload

---
+++ app.js
@@ -0,0 +1,50 @@
+// expres server
+// 1 page: homepage with question
+// grading happens with a scale useing radiobuttons 
+// post request send to json file
+// once post is done remove the send button from the page
+const express = require('express')
+const fs = require('fs')
+const app = express()
+const port = 3000
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.set('view engine', 'ejs')
+
+// get request for homepage
+// send back index page
+app.get('/', (req, res) => {
+	res.render('index')
+})
+
+app.post('/submit', (req, res) => {
+	console.log(req.body)
+	let userData = req.body
+	//read file
+	fs.readFile('data.json', (err,data) => {
+		if(err) { console.log(err)}
+		let item = JSON.parse(data)
+		console.log(item)
+		item.push(userData)
+		console.log(item)
+		item = JSON.stringify(item,null,2)
+		fs.writeFile('data.json', item, (err,data) => {
+			if(err){console.log(err)}
+			console.log('file written!')
+		})
+	})
+	
+	
+	//stringify
+	//write
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
+// post request to recieve the data
+// fs append to store the data
+// res.send data recieve
